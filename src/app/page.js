6534ce21a16a898ee23229f7f3f0a5c0436b63ab
AlanRoybal <alanroybal05@gmail.com>
2025-09-01 16:17:38 -0500

    boilerplate done

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
 import Image from "next/image";
+import Navbar from "./components/navbar";
+import Hero from "./components/hero";
+import Courses from "./components/courses";
 
 export default function Home() {
   return (
-    <div>Hi</div>
+    <div className="flex justify-center items-center min-h-screen">
+      Filler Text
+    </div>
   );
 }
