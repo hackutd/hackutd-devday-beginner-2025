0652c87644d9d4f0ccd694735e2984a6b36c5bd1
AlanRoybal <alanroybal05@gmail.com>
2025-09-07 01:09:27 -0500

    fixed coursecard dummy data

--- src/app/components/coursecard.js
+++ src/app/components/coursecard.js
@@ -1,17 +1,15 @@
 import React from 'react';
 
+const data = [
+  {
+    "courseCode": "CS 1200",
+    "title": "Introduction to Computer Science and Software Engineering",
+    "description": "Overview of computer science and software engineering concepts.",
+    "creditHours": 2
+  }
+];
+
 const CourseCard = () => {
-  const data = [
-    {
-      "courseCode": "CS 1200",
-      "title": "Introduction to Computer Science and Software Engineering",
-      "description": "Overview of computer science and software engineering concepts.",
-      "creditHours": 2
-    }
-  ];
-
-  const course = data[0];
-
   return (
     <div>
       
