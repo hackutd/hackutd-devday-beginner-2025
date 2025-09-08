42a43b441ee89615defbbcb96c3f2bd6d2246d56
AlanRoybal <alanroybal05@gmail.com>
2025-09-08 17:53:40 -0500

    fixed navbar text

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -5,7 +5,7 @@ import { User, Settings } from "lucide-react";
 function Navbar() {
     return (
         <nav className="flex justify-between items-center py-4 px-8 bg-gray-100 border-b border-gray-300">
-            <div className="flex gap-4">
+            <div className="flex gap-4 text-gray-900">
                 <Link href="/settings" className="group relative">
                     <Settings size={24} className="cursor-pointer text-gray-800" />
                     <span className="absolute left-0 -bottom-1 w-full h-0.5 bg-blue-500 scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left rounded-full pointer-events-none" />
@@ -19,7 +19,7 @@ function Navbar() {
                 </Link>
             </div>
 
-            <div className="flex gap-4">
+            <div className="flex gap-4 text-gray-900">
                 <Link href="/profile" className="group relative">
                     <User size={24} className="cursor-pointer text-gray-800" />
                     <span className="absolute left-0 -bottom-1 w-full h-0.5 bg-blue-500 scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left rounded-full pointer-events-none" />
